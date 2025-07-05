76e3157b89cdb12fcc4d7aed2d735623b53f5093
Shourya1911 <shouryasinha00@gmail.com>
2025-07-06 03:10:14 +0530

    setup project files - part 1

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
-const port = 4000
+const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/twitter',(req, r) => {})
+app.get('/twitter',(req, res) => {
+  res.send('shouryadotcom')
+})
 
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
