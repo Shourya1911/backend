6cdef6c3ba48e3fce884cecbaaaaef9918edb7c2
Shourya1911 <shouryasinha00@gmail.com>
2025-07-20 06:45:09 +0530

    all setup configured

--- index.js
+++ index.js
@@ -15,3 +15,36 @@ app.get('/twitter',(req, res) => {
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
+
+
+
+//require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
+
+//import mongoose from "mongoose";
+//import { DB_NAME } from "./constants";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(` Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!!! ", err);
+})
+
+
+
+
+
+
+
+
